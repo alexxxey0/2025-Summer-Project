e8bcce57537ffb5037d2db7f84636004b48ba951
alexxxey <aleksejs.gorlovics@gmail.com>
2025-07-05 16:23:44 +0300

    Inertia, React, Tailwind setup. Home component.

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return <div className="p-4">
+        <h1 className='text-red-500 font-bold text-4xl'>Welcome to our website!</h1>
+        <p>This is our 2025 summer project</p>
+        <p>Made by VanekBeats and alexxxey</p>
+    </div>
+}
