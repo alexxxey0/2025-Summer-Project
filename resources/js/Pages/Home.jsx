90d6f4c85b37332e4e8dcf365b81f81f70c0a6c1
vanja <vanjaaleskevic@mail.ru>
2025-07-05 18:30:50 +0300

    test-commit

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,5 +3,6 @@ export default function Home() {
         <h1 className='text-red-500 font-bold text-4xl'>Welcome to our website!</h1>
         <p>This is our 2025 summer project</p>
         <p>Made by VanekBeats and alexxxey</p>
+        <p>Yo programming companies, give me and alexxxey a job</p>
     </div>
 }
