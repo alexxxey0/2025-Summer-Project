d7735a318c482485e4eade37365d1028038486c3
vanja <vanjaaleskevic@mail.ru>
2025-07-09 01:46:09 +0300

    navbar buttons

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,31 @@
 export default function Home() {
-    return <div className="p-4">
-        <h1 className='text-red-500 font-bold text-4xl'>Welcome to our website!</h1>
-        <p>This is our 2025 summer project</p>
-        <p>Made by VanekBeats and alexxxey</p>
-        <p>Yo programming companies, give me and alexxxey a job</p>
+    return <div className=" font-bebas">
+
+        <div className="navbar flex flex-row justify-between">
+            
+            <div className="logo">
+                <h1 className="text-4xl ml-1">Stylish</h1>
+                <h5 className="text-xl ml-2">Online Store</h5>
+            </div>
+
+            <div className="navbar-buttons">
+
+                <a href="">Home</a>
+                <a href="">Men</a>
+                <a href="">Women</a>
+                <a href="">Page</a>
+                <a href="">Shop</a>
+                <a href="">Sale</a>
+                <a href="">Get PRO</a>
+
+                <div className="navbar-icons">
+                    <p>icon 1</p>
+                </div>
+
+            </div>
+
+        </div>
+        
+
     </div>
 }
