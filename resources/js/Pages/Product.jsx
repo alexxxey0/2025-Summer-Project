ebad0e2aca2b8060065868fbd8492f1ffef4139d
alexxxey <aleksejs.gorlovics@gmail.com>
2025-07-21 15:34:12 +0300

    Added description, price, product details, total price for the number of items elected to Product page.

--- resources/js/Pages/Product.jsx
+++ resources/js/Pages/Product.jsx
@@ -16,7 +16,7 @@ function Product({ product }) {
 
     // If the size is in the weights array (which contains letter sizes), get the weight for this size for comparison.
     // Otherwise, if the size is not in the weights table, it must be a number, so compare it as a number.
-    // This way, we can sort both letter sizes (S, M, L...) and numeric size (40, 41, 42, 43...)
+    // This way, we can sort both letter sizes (S, M, L...) and numeric sizes (40, 41, 42, 43...).
     sizes.sort((a, b) => (weights[a] ? weights[a] : Number(a)) - (weights[b] ? weights[b] : Number(b)));
 
     const [selectedSize, setSelectedSize] = useState(product.sizes[0]);
@@ -25,7 +25,7 @@ function Product({ product }) {
 
     function handleSizeButtonClick(e) {
         setSelectedSize(e.target.value);
-        setQuantity(1);
+        setQuantity(1); // Reset the quantity to 1 when user changes sizes
     }
 
     function add_to_cart() {
@@ -36,11 +36,14 @@ function Product({ product }) {
         <div className="my-8">
             <h1 className="text-center font-bold text-3xl">{product.name}</h1>
 
-            <div className="flex flex-row w-10/12 mx-auto mt-16">
-                <div className="w-3/12 flex flex-col items-center">
+            <div className="flex flex-row gap-x-32 w-10/12 mx-auto mt-16">
+                <div className="w-4/12 flex flex-col items-center">
                     <img className="w-full" src={product.main_image_path} alt="Product main image" />
+
+                    {/* Size buttons */}
                     <div className="flex flex-row justify-center gap-x-2 mt-8 w-8/12 *:w-3/12">
                         {sizes.map((size) => {
+                            // The button that is currently selected is rendered in a different color (by passing the selected prop)
                             return selectedSize === size ?
                                 <SizeButton key={size} value={size} onClick={handleSizeButtonClick} selected={true}></SizeButton>
                                 :
@@ -48,8 +51,11 @@ function Product({ product }) {
                         }
                         )}
                     </div>
+
+                    {/* In stock, Total price, Add to cart, Quantity */}
                     <div className="flex flex-col w-full mt-16 items-start gap-y-2">
                         <p>In stock: {product.in_stock[selectedSize] ? product.in_stock[selectedSize] : 0}</p>
+                        <p>Total price: {(product.price * quantity).toFixed(2).toString().replace(".", ",") + " €"}</p>
                         <div className="flex flex-row gap-x-4 w-full">
                             <button className="bg-black text-white rounded-xl px-2 py-2 w-4/12 cursor-pointer h-10/12" onClick={add_to_cart}>Add to cart</button>
                             <div className="flex flex-col">
@@ -58,9 +64,33 @@ function Product({ product }) {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div>
 
+                </div>
+
+                {/* Description, Price, Product details */}
+                <div className="flex flex-col w-8/12 gap-y-8">
+                    <p className="border-2 rounded-xl p-2">{product.description}</p>
+                    <p className="font-bold text-xl">{product.price.replace(".", ",")} €</p>
+
+                    <div className="flex flex-row gap-x-20 text-lg">
+                        <div className="flex flex-col font-bold gap-y-2">
+                            <p>Manufacturer</p>
+                            <p>Type</p>
+                            <p>Color</p>
+                            <p>Gender</p>
+                            <p>Age category</p>
+                            <p>Season</p>
+                        </div>
+                        <div className="flex flex-col gap-y-2">
+                            <p>{product.manufacturer}</p>
+                            <p>{product.type}</p>
+                            <p>{product.color}</p>
+                            <p>{product.gender}</p>
+                            <p>{product.age_category}</p>
+                            <p>{product.season}</p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
