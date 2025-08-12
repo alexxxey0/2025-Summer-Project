f1d13de26e830fd9a378b234b6530f3c389cfb40
alexxxey <aleksejs.gorlovics@gmail.com>
2025-08-12 18:59:33 +0300

    Little bit of responsive design.

--- resources/js/Pages/Admin/AdminPanelTabs/ManageProducts.jsx
+++ resources/js/Pages/Admin/AdminPanelTabs/ManageProducts.jsx
@@ -120,8 +120,8 @@ function ManageProducts(props) {
             <p>Click on a row to get more detailed information about a product and edit it.</p>
 
             {/* Search products, Get product by ID */}
-            <div className="flex flex-row gap-x-8 items-end">
-                <div className="border-2 rounded-lg p-2 w-4/12">
+            <div className="flex flex-col xl:flex-row gap-x-8 items-start xl:items-end gap-y-4">
+                <div className="border-2 rounded-lg p-2 w-10/12 xl:w-4/12">
                     <h1 className="font-bold mb-4 text-lg">Search products</h1>
                     <div className="search_products flex flex-col gap-y-4">
                         <AdminPanelInput onChange={handleSearch} name="name" />
@@ -129,7 +129,7 @@ function ManageProducts(props) {
 
                         <div className="flex flex-col gap-y-4 mt-4">
                             <p>Price</p>
-                            <div className="text-xs self-center w-11/12">
+                            <div className="text-xs self-center w-11/12 hidden xl:block">
                                 <Slider range value={priceRange} min={0} max={maxSliderValue} step={10} allowCross={false} marks={marks}
                                     onChange={(e) => { onSliderChange(e) }}
                                 ></Slider>
@@ -160,7 +160,7 @@ function ManageProducts(props) {
                 </div>
             </div>
 
-            <Link href="/add_new_product_page" className="flex flex-row items-center gap-x-2 w-2/12 bg-black text-white px-4 py-2 text-xl rounded-lg shadow active:translate-y-0.5 active:shadow-inner hover:scale-105 transition cursor-pointer">+ Add new product</Link>
+            <Link href="/add_new_product_page" className="flex flex-row items-center gap-x-2 w-6/12 xl:w-2/12 bg-black text-white px-4 py-2 text-xl rounded-lg shadow active:translate-y-0.5 active:shadow-inner hover:scale-105 transition cursor-pointer">+ Add new product</Link>
 
             {/* Products table */}
             <div className="grid grid-cols-5 bg-[#5b5b5b]  *:p-2 *:overflow-x-scroll gap-0.5 border-2 border-[#5b5b5b] rounded">
