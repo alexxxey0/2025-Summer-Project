801b72f0cd8277e1b4ea82d45e81a1c712488a89
alexxxey <aleksejs.gorlovics@gmail.com>
2025-08-14 20:44:41 +0300

    cart_items table. Migration and model.

---
+++ database/migrations/2025_08_14_202139_create_cart_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id('cart_item_id');
+            $table->unsignedBigInteger('product_variant_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('product_variant_id')->references('product_variant_id')->on('product_variants')->onDelete('cascade');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
