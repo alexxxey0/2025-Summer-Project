3f0564fc4c0c1192ee4ad6d5a3c6a0873591bd11
alexxxey <aleksejs.gorlovics@gmail.com>
2025-08-22 10:39:10 +0300

    Displaying actual product from the cart on the Cart page.

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -1,8 +1,17 @@
+import React, { useContext } from 'react';
 import { usePage } from '@inertiajs/react';
 import { Link } from "@inertiajs/react";
+import { CartItemsContext } from './components/Layout';
 
 function Cart() {
     const { asset_path } = usePage().props;
+    const { cartItems, setCartItems } = useContext(CartItemsContext);
+
+    let cartTotalPrice = 0;
+    cartItems.forEach(cartItem => {
+        cartTotalPrice += Number(cartItem.total_price);
+    });
+    // console.log(cartItems);
 
     return (
         <div className="flex flex-col mt-10 justify-center mb-20">
@@ -22,46 +31,23 @@ function Cart() {
                     <div className="text-center">Price</div>
                 </div>
 
-                {/* 1st product */}
-                <div className="grid grid-cols-[300px_1fr_150px_100px_150px] justify-items-center border-b py-6 items-center">
-                    <img className="object-cover w-[300px]" src={asset_path + "images/vans.jpg"} alt="" />
-                    <div>
-                        <h3 className="font-bold">Vans Old Skool</h3>
-                        <h4 className="text-gray-500">Size: 43</h4>
+                {cartItems.map((cartItem) =>
+                    <div key={cartItem.cart_item_id} className="grid grid-cols-[300px_1fr_150px_100px_150px] justify-items-center border-b py-6 items-center">
+                        <img className="object-cover w-[300px]" src={cartItem.image_path} alt="" />
+                        <div>
+                            <h3 className="font-bold">{cartItem.name}</h3>
+                            <h4 className="text-gray-500">Size: {cartItem.size}</h4>
+                        </div>
+                        <div className="text-center">{cartItem.price} €</div>
+                        <div className="text-center">{cartItem.quantity}</div>
+                        <div className="text-center">{cartItem.total_price} €</div>
                     </div>
-                    <div className="text-center">121,00$</div>
-                    <div className="text-center">1</div>
-                    <div className="text-center">121,00$</div>
-                </div>
-
-                {/* 2nd product */}
-                <div className="grid grid-cols-[300px_1fr_150px_100px_150px] justify-items-center border-b py-6 items-center">
-                    <img className="object-cover w-[300px]" src={asset_path + "images/vans.jpg"} alt="" />
-                    <div>
-                        <h3 className="font-bold">Vans Old Skool</h3>
-                        <h4 className="text-gray-500">Size: 43</h4>
-                    </div>
-                    <div className="text-center">121,00$</div>
-                    <div className="text-center">1</div>
-                    <div className="text-center">121,00$</div>
-                </div>
-
-                {/* 3rd product */}
-                <div className="grid grid-cols-[300px_1fr_150px_100px_150px] justify-items-center border-b py-6 items-center">
-                    <img className="object-cover w-[300px]" src={asset_path + "images/vans.jpg"} alt="" />
-                    <div>
-                        <h3 className="font-bold">Vans Old Skool</h3>
-                        <h4 className="text-gray-500">Size: 43</h4>
-                    </div>
-                    <div className="text-center">121,00$</div>
-                    <div className="text-center">1</div>
-                    <div className="text-center">121,00$</div>
-                </div>
+                )}
 
                 {/* Lower panel with total price */}
                 <div className="flex justify-end items-center mt-6 gap-4">
                     <p className="text-lg font-bold">
-                        Total: <span className="font-normal">$363.00</span>
+                        Total: <span className="font-normal">{cartTotalPrice.toFixed(2)} €</span>
                     </p>
                     <Link
                         href=""
